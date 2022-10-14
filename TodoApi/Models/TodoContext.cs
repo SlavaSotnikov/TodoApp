504cb8b0a2088d550b3a07b6e7ca4083b71c5713
slava-pc\Slava <slavasotnikov2020@ukr.net>
2022-10-14 18:47:07 +0300

    Created TodoItemsAPI

---
+++ TodoApi/Models/TodoContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoApi.Models
+{
+    public class TodoContext : DbContext
+    {
+        protected readonly IConfiguration Config;
+        public TodoContext(IConfiguration cofig)
+        {
+            Config = cofig;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseNpgsql(Config.GetConnectionString("PostgresConnection"));
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; } = null!;
+    }
+}
